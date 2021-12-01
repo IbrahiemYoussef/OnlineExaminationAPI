0407ea6dbac365b1e9fa4322ecfa931961ba6b7b
MohammedAbusarie <new.is.new55@gmail.com>
2021-12-01 14:07:14 +0200

    Added class Answer

---
+++ FinalYearProject/Data/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalYearProject.Data
+{
+    class Answer
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+    }
+}
